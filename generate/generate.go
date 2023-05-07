3522f161b3da86d0ab46c92b699de128646f2129
Pavel Milko <p.milko1999@yandex.ru>
2023-05-07 14:08:59 +0300

    Move files

---
+++ generate/generate.go
@@ -0,0 +1,25 @@
+package generate
+
+import "github.com/Feresey/mtest/schema"
+
+type Generator struct {
+	g *schema.Graph
+}
+
+func New(g *schema.Graph) *Generator {
+	return &Generator{g: g}
+}
+
+func (g *Generator) Generate() error {
+	tablesOrder, err := g.buildTablesInsertOrder()
+	if err != nil {
+		return err
+	}
+
+	_ = tablesOrder
+	return nil
+}
+
+func (g *Generator) buildTablesInsertOrder() ([]string, error) {
+	g.g.Graph[]
+}
---
+++ parse/queries/sql/types.sql
@@ -0,0 +1,48 @@
+SELECT
+	ns.nspname      AS schema_name,
+	t.typname       AS type_name,
+	t.typtype::TEXT AS type_type,
+	-- array types
+	t.typcategory = 'A' AS is_array,
+	et_ns.nspname       AS element_type_shema,
+	et.typname          AS element_type_name,
+	-- domain types
+	t.typnotnull AS domain_is_not_nullable,
+	nd.nspname   AS domain_schema,
+	dt.typname   AS domain_type,
+	information_schema._pg_char_max_length(dt.oid, t.typtypmod)::INT AS domain_character_max_length,
+	t.typndims   AS domain_array_dims,
+	-- range types
+	rst_ns.nspname AS range_element_type_schema,
+	rst.typname    AS range_element_type_name,
+	rmt_ns.nspname AS range_multi_element_type_schema,
+	rmt.typname    AS range_multi_type_name,
+	-- enum types
+	(SELECT
+		COALESCE(
+			array_agg(e.enumlabel::TEXT),
+			'{}'::TEXT[]
+		) AS enum_values
+	FROM
+		pg_type t
+		JOIN pg_enum e ON t.oid = e.enumtypid
+		JOIN pg_namespace n ON n.oid = t.typnamespace
+	WHERE
+		n.nspname || '.' || t.typname = ANY($1)
+	GROUP BY
+		t.oid
+	) AS enum_values
+FROM
+	pg_type t
+	JOIN pg_namespace ns          ON t.typnamespace = ns.oid
+	LEFT JOIN pg_type et          ON t.typelem = et.oid
+	LEFT JOIN pg_namespace et_ns   ON et.typnamespace = et_ns.oid
+	LEFT JOIN pg_type dt          ON t.typbasetype = dt.oid
+	LEFT JOIN pg_namespace nd     ON dt.typnamespace = nd.oid
+	LEFT JOIN pg_range rng        ON rng.rngtypid = t.oid
+	LEFT JOIN pg_type rst         ON rng.rngsubtype = rst.oid
+	LEFT JOIN pg_namespace rst_ns ON rst.typnamespace = rst_ns.oid
+	LEFT JOIN pg_type rmt         ON rng.rngmultitypid = rmt.oid
+	LEFT JOIN pg_namespace rmt_ns ON rmt.typnamespace = rmt_ns.oid
+WHERE
+	ns.nspname || '.' || t.typname = ANY($1)
